cce3e57a0768a3e623e07e5822e2e339882e3287
PhranakornSoft Co., Ltd <watcharapongw@phranakornsoft.com>
2018-10-25 19:17:05 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,8 +1,8 @@
 <?php
 
-$access_token = 'DnSrl9Jx9teqxIr1AgarhmcdvORQuWAOtV7aw7GXXbYMm0ED6D2rTcB9bL066Lb976+eRXK3rLJFFQLUevXr0fPFq+h3qAE6b6tnZmt/dxxFYkbtmskgha/SZbQiCgkgUXvt/KW1v9TSFoJFZhi+7AdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ggO5i0vjS5wZGH9Lk5DuXy23fVC3M6jIfo12fasF0O7zzPMeKv4mwyen+y8wK4SH76+eRXK3rLJFFQLUevXr0fPFq+h3qAE6b6tnZmt/dxwsK6iH7N7WKQsq5C2Xd3pwmqAAliqiWrBF5fh7wMZuDwdB04t89/1O/w1cDnyilFU=';
 
-$userId = '7e1c3a3c9f8a3b50461ad20aadf0eaa1';
+$userId = 'U3c4f306fd28e58ce9cec852d82a26093';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
