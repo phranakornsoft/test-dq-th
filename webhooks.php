9a31190c35ed542010db580f4f239d3a0fbd115b
PhranakornSoft Co., Ltd <dev@phranakornsoft.com>
2018-10-26 12:38:49 +0700

    add new bot

--- webhooks.php
+++ webhooks.php
@@ -74,7 +74,7 @@ function replyMsg($arrayHeader,$arrayPostData){
 	curl_setopt($ch, CURLOPT_URL,$strUrl);
 	curl_setopt($ch, CURLOPT_HEADER, false);
 	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
 	curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
