16ff94399adbf9ad4a14cc2abe034b68175b90d3
PhranakornSoft Co., Ltd <dev@phranakornsoft.com>
2018-10-26 11:28:24 +0700

    add card test

--- webhooks.php
+++ webhooks.php
@@ -58,6 +58,22 @@ else if($message == "ลาก่อน"){
     $arrayPostData['messages'][1]['stickerId'] = "131";
     replyMsg($arrayHeader,$arrayPostData);
 }
+#Test Card
+else if($message == "เมนูแนะนำ"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "carousel";
+    $arrayPostData['messages'][0]['thumbnailImageUrl'] = "https://res.cloudinary.com/ginja-co-ltd/image/upload/s--bOVLNxnY--/c_fill,h_300,q_jpegmini,w_485/v1/brands/6/inventory/products/18592-coconut-with-sticky-rice-blizz-p7inqy";
+    $arrayPostData['messages'][0]['title'] = "บลิซซาร์ด® มะพร้าวน้ำหอมกับเนื้อมะพร้าวคั่ว x2";
+    $arrayPostData['messages'][0]['text'] = "บลิซซาร์ด® มะพร้าวน้ำหอมกับเนื้อมะพร้าวคั่ว ขนาด XL 2 แก้ว และน้ำแข็งแห้ง";
+    $arrayPostData['messages'][0]['actions'] = [
+            {
+              "type": "message",
+              "label": "฿ 45",
+              "text": "Product 1"
+            };
+        ]
+    replyMsg($arrayHeader,$arrayPostData);
+}
 
 function replyMsg($arrayHeader,$arrayPostData){
     $strUrl = "https://api.line.me/v2/bot/message/reply";
