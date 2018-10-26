890d841fa6ac125c82d580874644e91c711f7dc5
PhranakornSoft Co., Ltd <dev@phranakornsoft.com>
2018-10-26 15:34:51 +0700

    edit user id

--- webhooks.php
+++ webhooks.php
@@ -57,7 +57,7 @@ $message = $arrayJson['events'][0]['message']['text'];
 if($message == "สวัสดี"){
     $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
     $arrayPostData['messages'][0]['type'] = "text";
-    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา".$id;
     replyMsg($arrayHeader,$arrayPostData);
 }
 #ตัวอย่าง Message Type "Sticker"
@@ -121,6 +121,18 @@ else if($message == "สินค้าใหม่"){
 	}
 	$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
 	$arrayPostData = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
+
+	replyMsg($arrayHeader,$arrayPostData);
+}
+// Check Location User
+elseif ($arrayJson['events'][0]['message']['text']=="") {
+
+	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+	$arrayPostData['messages'][0]['type'] = "location";
+	$arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+	$arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+	$arrayPostData['messages'][0]['latitude'] = "13.7465354";
+	$arrayPostData['messages'][0]['longitude'] = "100.532752";
 	replyMsg($arrayHeader,$arrayPostData);
 }
 
