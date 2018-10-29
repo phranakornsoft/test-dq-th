7f92283600d7462fa0894b037f9d5b766c6a11ed
PhranakornSoft Co., Ltd <watcharapongw@phranakornsoft.com>
2018-10-29 19:17:49 +0700

    Update profile.php
    del key

--- profile.php
+++ profile.php
@@ -1,8 +1,8 @@
 <?php
 
-$access_token = 'ggO5i0vjS5wZGH9Lk5DuXy23fVC3M6jIfo12fasF0O7zzPMeKv4mwyen+y8wK4SH76+eRXK3rLJFFQLUevXr0fPFq+h3qAE6b6tnZmt/dxwsK6iH7N7WKQsq5C2Xd3pwmqAAliqiWrBF5fh7wMZuDwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'XXXXXX';
 
-$userId = 'U3c4f306fd28e58ce9cec852d82a26093';
+$userId = 'XXXXXX';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
