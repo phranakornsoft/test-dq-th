a40ccbc93c3e2503f170059c84e69556143d082e
PhranakornSoft Co., Ltd <watcharapongw@phranakornsoft.com>
2018-10-29 19:18:06 +0700

    Update verify.php
    del key

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'ggO5i0vjS5wZGH9Lk5DuXy23fVC3M6jIfo12fasF0O7zzPMeKv4mwyen+y8wK4SH76+eRXK3rLJFFQLUevXr0fPFq+h3qAE6b6tnZmt/dxwsK6iH7N7WKQsq5C2Xd3pwmqAAliqiWrBF5fh7wMZuDwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'XXXXXX';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
