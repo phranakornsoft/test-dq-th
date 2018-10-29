bdbae4fd3c08cc3d90db1f2a04bf6ebe076958d7
PhranakornSoft Co., Ltd <watcharapongw@phranakornsoft.com>
2018-10-29 19:17:31 +0700

    Update bots.php
    del key

--- bots.php
+++ bots.php
@@ -39,8 +39,8 @@
 	use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 	use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 
-	$httpClient = new CurlHTTPClient('ggO5i0vjS5wZGH9Lk5DuXy23fVC3M6jIfo12fasF0O7zzPMeKv4mwyen+y8wK4SH76+eRXK3rLJFFQLUevXr0fPFq+h3qAE6b6tnZmt/dxwsK6iH7N7WKQsq5C2Xd3pwmqAAliqiWrBF5fh7wMZuDwdB04t89/1O/w1cDnyilFU=');
-	$bot = new LINEBot($httpClient, array('channelSecret' => '9bcd4924ed3df67281e6ba22b9a7a5c7'));
+	$httpClient = new CurlHTTPClient('XXXXXX');
+	$bot = new LINEBot($httpClient, array('channelSecret' => 'XXXXXX'));
 
 	 
 	// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
